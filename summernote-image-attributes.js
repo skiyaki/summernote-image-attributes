9f27273a5e1bd0777386b3e7d15c8d163934990a
togashi-skiyaki <togashi@skiyaki.com>
2019-08-08 12:51:02 +0900

    account用

--- summernote-image-attributes.js
+++ summernote-image-attributes.js
@@ -22,11 +22,11 @@
     }).promise();
   };
   $.extend(true,$.summernote.lang, {
-    'en-US': { /* US English(Default Language) */
+    'ja-JP': { /* JP Japanese(Default Language) */
       imageAttributes: {
-        dialogTitle: 'Image Attributes',
-        tooltip: 'Image Attributes',
-        tabImage: 'Image',
+        dialogTitle: '詳細設定',
+        tooltip: '詳細設定',
+        tabImage: '画像',
           src: 'Source',
           browse: 'Browse',
           title: 'Title',
@@ -36,10 +36,10 @@
           class: 'Class',
           style: 'Style',
           role: 'Role',
-        tabLink: 'Link',
+        tabLink: 'リンク',
           linkHref: 'URL',
           linkTarget: 'Target',
-          linkTargetInfo: 'Options: _self, _blank, _top, _parent',
+          linkTargetInfo: '選択可能: _self, _blank, _top, _parent',
           linkClass: 'Class',
           linkStyle: 'Style',
           linkRel: 'Rel',
@@ -89,37 +89,14 @@
         var $container = options.dialogsInBody ? $(document.body) : $editor;
         var timestamp = Date.now();
         var body = '<ul class="nav note-nav nav-tabs note-nav-tabs">' +
-                   '  <li class="active"><a href="#note-imageAttributes' + timestamp + '" data-toggle="tab">' + lang.imageAttributes.tabImage + '</a></li>' +
-                   '  <li><a href="#note-imageAttributes-attributes' + timestamp + '" data-toggle="tab">' + lang.imageAttributes.tabAttributes + '</a></li>' +
-                   '  <li><a href="#note-imageAttributes-link' + timestamp + '" data-toggle="tab">' + lang.imageAttributes.tabLink + '</a></li>';
+                   '  <li class="nav-item"><a class="nav-link active" href="#note-imageAttributes-link' + timestamp + '" data-toggle="tab">' + lang.imageAttributes.tabLink + '</a></li>';
         if (options.imageAttributes.disableUpload == false) {
            body += '  <li><a href="#note-imageAttributes-upload' + timestamp + '" data-toggle="tab">' + lang.imageAttributes.tabUpload + '</a></li>';
         }
         body +=    '</ul>' +
                    '<div class="tab-content note-tab-content">' +
-                   // Tab 2
-                   '  <div class="tab-pane note-tab-pane" id="note-imageAttributes-attributes' + timestamp + '">' +
-                   '    <div class="note-form-group form-group note-group-imageAttributes-class">' +
-                   '      <label class="control-label note-form-label col-sm-3">' + lang.imageAttributes.class + '</label>' +
-                   '      <div class="input-group note-input-group col-xs-12 col-sm-9">' +
-                   '        <input class="note-imageAttributes-class form-control note-form-control note-input" type="text">' +
-                   '      </div>' +
-                   '    </div>' +
-                   '    <div class="note-form-group form-group note-group-imageAttributes-style">' +
-                   '      <label class="control-label note-form-label col-sm-3">' + lang.imageAttributes.style + '</label>' +
-                   '      <div class="input-group note-input-group col-xs-12 col-sm-9">' +
-                   '        <input class="note-imageAttributes-style form-control note-form-control note-input" type="text">' +
-                   '      </div>' +
-                   '    </div>' +
-                   '    <div class="note-form-group form-group note-group-imageAttributes-role">' +
-                   '      <label class="control-label note-form-label col-sm-3">' + lang.imageAttributes.role + '</label>' +
-                   '      <div class="input-group note-input-group col-xs-12 col-sm-9">' +
-                   '        <input class="note-imageAttributes-role form-control note-form-control note-input" type="text">' +
-                   '      </div>' +
-                   '    </div>' +
-                   '  </div>' +
                    // Tab 3
-                   '  <div class="tab-pane note-tab-pane" id="note-imageAttributes-link' + timestamp + '">' +
+                   '  <div class="tab-pane note-tab-pane fade in active show" id="note-imageAttributes-link' + timestamp + '">' +
                    '    <div class="note-form-group form-group note-group-imageAttributes-link-href">' +
                    '      <label class="control-label note-form-label col-xs-3">' + lang.imageAttributes.linkHref + '</label>' +
                    '      <div class="input-group note-input-group col-xs-12 col-sm-9">' +
@@ -132,31 +109,6 @@
                    '        <input class="note-imageAttributes-link-target form-control note-form-control note-input" type="text">' +
                    '      </div>' +
                    '      <small class="help-block note-help-block text-right">' + lang.imageAttributes.linkTargetInfo + '</small>' +
-                   '    </div>' +
-                   '    <div class="note-form-group form-group note-group-imageAttributes-link-class">' +
-                   '      <label class="control-label note-form-label col-xs-3">' + lang.imageAttributes.linkClass + '</label>' +
-                   '      <div class="input-group note-input-group col-xs-12 col-sm-9">' +
-                   '        <input class="note-imageAttributes-link-class form-control note-form-control note-input" type="text">' +
-                   '      </div>' +
-                   '    </div>' +
-                   '    <div class="note-form-group form-group note-group-imageAttributes-link-style">' +
-                   '      <label class="control-label note-form-label col-xs-3">' + lang.imageAttributes.linkStyle + '</label>' +
-                   '      <div class="input-group note-input-group col-xs-12 col-sm-9">' +
-                   '        <input class="note-imageAttributes-link-style form-control note-form-control note-input" type="text">' +
-                   '      </div>' +
-                   '    </div>' +
-                   '    <div class="note-form-group form-group note-group-imageAttributes-link-rel">' +
-                   '      <label class="control-label note-form-label col-xs-3">' + lang.imageAttributes.linkRel + '</label>' +
-                   '      <div class="input-group note-input-group col-xs-12 col-sm-9">' +
-                   '        <input class="note-imageAttributes-link-rel form-control note-form-control note-input" type="text">' +
-                   '      </div>' +
-                   '      <small class="help-block note-help-block text-right">' + lang.imageAttributes.linkRelInfo + '</small>' +
-                   '    </div>' +
-                   '    <div class="note-form-group form-group note-group-imageAttributes-link-role">' +
-                   '      <label class="control-label note-form-label col-xs-3">' + lang.imageAttributes.linkRole + '</label>' +
-                   '      <div class="input-group note-input-group col-xs-12 col-sm-9">' +
-                   '        <input class="note-imageAttributes-link-role form-control note-form-control note-input" type="text">' +
-                   '      </div>' +
                    '    </div>' +
                    '  </div>';
       if (options.imageAttributes.disableUpload == false) {
@@ -178,26 +130,6 @@
 //                   '        <span class="input-group-btn">' +
 //                   '          <button class="btn btn-default class="note-imageAttributes-browse">' + lang.imageAttributes.browse + '</button>' +
 //                   '        </span>' +
-                   '      </div>' +
-                   '    </div>' +
-                   '    <div class="note-form-group form-group note-group-imageAttributes-title">' +
-                   '      <label class="control-label note-form-label col-sm-3">' + lang.imageAttributes.title + '</label>' +
-                   '      <div class="input-group note-input-group col-xs-12 col-sm-9">' +
-                   '        <input class="note-imageAttributes-title form-control note-form-control note-input" type="text" />' +
-                   '      </div>' +
-                   '    </div>' +
-                   '    <div class="note-form-group form-group note-group-imageAttributes-alt">' +
-                   '      <label class="control-label note-form-label col-sm-3">' + lang.imageAttributes.alt + '</label>' +
-                   '      <div class="input-group note-input-group col-xs-12 col-sm-9">' +
-                   '        <input class="note-imageAttributes-alt form-control note-form-control note-input" type="text" />' +
-                   '      </div>' +
-                   '    </div>' +
-                   '    <div class="note-form-group form-group note-group-imageAttributes-dimensions">' +
-                   '      <label class="control-label note-form-label col-sm-3">' + lang.imageAttributes.dimensions + '</label>' +
-                   '      <div class="input-group note-input-group col-xs-12 col-sm-9">' +
-                   '        <input class="note-imageAttributes-width form-control note-form-control note-input" type="text" />' +
-                   '        <span class="input-group-addon note-input-group-addon">x</span>' +
-                   '        <input class="note-imageAttributes-height form-control note-form-control note-input" type="text" />' +
                    '      </div>' +
                    '    </div>' +
                    '  </div>' +
@@ -227,46 +159,21 @@
         var $img    = $($editable.data('target'));
         var imgInfo = {
           imgDom:  $img,
-          title:   $img.attr('title'),
           src:     $img.attr('src'),
-          alt:     $img.attr('alt'),
-          width:   $img.attr('width'),
-          height:  $img.attr('height'),
-          role:    $img.attr('role'),
-          class:   $img.attr('class'),
-          style:   $img.attr('style'),
           imgLink: $($img).parent().is("a") ? $($img).parent() : null
         };
         this.showImageAttributesDialog(imgInfo).then( function (imgInfo) {
           ui.hideDialog(self.$dialog);
           var $img = imgInfo.imgDom;
           if (options.imageAttributes.removeEmpty) {
-            if (imgInfo.alt)    $img.attr('alt',   imgInfo.alt);    else $img.removeAttr('alt');
-            if (imgInfo.width)  $img.attr('width', imgInfo.width);  else $img.removeAttr('width');
-            if (imgInfo.height) $img.attr('height',imgInfo.height); else $img.removeAttr('height');
-            if (imgInfo.title)  $img.attr('title', imgInfo.title);  else $img.removeAttr('title');
             if (imgInfo.src)    $img.attr('src',   imgInfo.src);    else $img.attr('src', '#');
-            if (imgInfo.class)  $img.attr('class', imgInfo.class);  else $img.removeAttr('class');
-            if (imgInfo.style)  $img.attr('style', imgInfo.style);  else $img.removeAttr('style');
-            if (imgInfo.role)   $img.attr('role',  imgInfo.role);   else $img.removeAttr('role');
           } else {
             if (imgInfo.src)    $img.attr('src',   imgInfo.src);    else $img.attr('src', '#');
-            $img.attr('alt',    imgInfo.alt);
-            $img.attr('width',  imgInfo.width);
-            $img.attr('height', imgInfo.height);
-            $img.attr('title',  imgInfo.title);
-            $img.attr('class',  imgInfo.class);
-            $img.attr('style',  imgInfo.style);
-            $img.attr('role',   imgInfo.role);
           }
           if($img.parent().is("a")) $img.unwrap();
           if (imgInfo.linkHref) {
             var linkBody = '<a';
-            if (imgInfo.linkClass) linkBody += ' class="' + imgInfo.linkClass + '"';
-            if (imgInfo.linkStyle) linkBody += ' style="' + imgInfo.linkStyle + '"';
             linkBody += ' href="' + imgInfo.linkHref + '" target="' + imgInfo.linkTarget + '"';
-            if (imgInfo.linkRel) linkBody += ' rel="' + imgInfo.linkRel + '"';
-            if (imgInfo.linkRole) linkBody += ' role="' + imgInfo.linkRole + '"';
             linkBody += '></a>';
             $img.wrap(linkBody);
           }
@@ -276,35 +183,16 @@
       };
       this.showImageAttributesDialog = function (imgInfo) {
         return $.Deferred( function (deferred) {
-          var $imageTitle  = self.$dialog.find('.note-imageAttributes-title'),
-              $imageInput  = self.$dialog.find('.note-imageAttributes-input'),
+          var $imageInput  = self.$dialog.find('.note-imageAttributes-input'),
               $imageSrc    = self.$dialog.find('.note-imageAttributes-src'),
-              $imageAlt    = self.$dialog.find('.note-imageAttributes-alt'),
-              $imageWidth  = self.$dialog.find('.note-imageAttributes-width'),
-              $imageHeight = self.$dialog.find('.note-imageAttributes-height'),
-              $imageClass  = self.$dialog.find('.note-imageAttributes-class'),
-              $imageStyle  = self.$dialog.find('.note-imageAttributes-style'),
-              $imageRole   = self.$dialog.find('.note-imageAttributes-role'),
               $linkHref    = self.$dialog.find('.note-imageAttributes-link-href'),
               $linkTarget  = self.$dialog.find('.note-imageAttributes-link-target'),
-              $linkClass   = self.$dialog.find('.note-imageAttributes-link-class'),
-              $linkStyle   = self.$dialog.find('.note-imageAttributes-link-style'),
-              $linkRel     = self.$dialog.find('.note-imageAttributes-link-rel'),
-              $linkRole    = self.$dialog.find('.note-imageAttributes-link-role'),
               $editBtn     = self.$dialog.find('.note-imageAttributes-btn');
           $linkHref.val();
-          $linkClass.val();
-          $linkStyle.val();
-          $linkRole.val();
           $linkTarget.val();
-          $linkRel.val();
           if (imgInfo.imgLink) {
             $linkHref.val(imgInfo.imgLink.attr('href'));
-            $linkClass.val(imgInfo.imgLink.attr('class'));
-            $linkStyle.val(imgInfo.imgLink.attr('style'));
-            $linkRole.val(imgInfo.imgLink.attr('role'));
             $linkTarget.val(imgInfo.imgLink.attr('target'));
-            $linkRel.val(imgInfo.imgLink.attr('rel'));
           }
           ui.onDialogShown(self.$dialog, function () {
             context.triggerEvent('dialog.shown');
@@ -326,32 +214,14 @@
               e.preventDefault();
               deferred.resolve({
                 imgDom:     imgInfo.imgDom,
-                title:      $imageTitle.val(),
                 src:        $imageSrc.val(),
-                alt:        $imageAlt.val(),
-                width:      $imageWidth.val(),
-                height:     $imageHeight.val(),
-                class:      $imageClass.val(),
-                style:      $imageStyle.val(),
-                role:       $imageRole.val(),
                 linkHref:   $linkHref.val(),
-                linkTarget: $linkTarget.val(),
-                linkClass:  $linkClass.val(),
-                linkStyle:  $linkStyle.val(),
-                linkRel:    $linkRel.val(),
-                linkRole:   $linkRole.val()
+                linkTarget: $linkTarget.val()
               }).then(function (img) {
                 context.triggerEvent('change', $editable.html());
               });
             });
-            $imageTitle.val(imgInfo.title);
             $imageSrc.val(imgInfo.src);
-            $imageAlt.val(imgInfo.alt);
-            $imageWidth.val(imgInfo.width);
-            $imageHeight.val(imgInfo.height);
-            $imageClass.val(imgInfo.class);
-            $imageStyle.val(imgInfo.style);
-            $imageRole.val(imgInfo.role);
             self.bindEnterKey($editBtn);
             self.bindLabels();
           });
