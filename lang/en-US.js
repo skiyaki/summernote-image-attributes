bd3f72168398f5759484c819d06f0f982c20d94e
akaishi-skiyaki <akaishi@skiyaki.com>
2020-11-24 18:31:51 +0900

    多言語対応
    デフォルトが日本語に切り替わって利用されているので英語の追加
    韓国語は本家が対応したので追従

---
+++ lang/en-US.js
@@ -0,0 +1,31 @@
+$.extend(true,$.summernote.lang,{
+  'en-US':{ /* Spanish */
+    imageAttributes:{
+      dialogTitle: 'Image Attributes',
+      tooltip: 'Image Attributes',
+      tabImage: 'Image',
+        src: 'Source',
+        browse: 'Browse',
+        title: 'Title',
+        alt: 'Alt Text',
+        dimensions: 'Dimensions',
+      tabAttributes: 'Attributes',
+        class: 'Class',
+        style: 'Style',
+        role: 'Role',
+      tabLink: 'Link',
+        linkHref: 'URL',
+        linkTarget: 'Target',
+        linkTargetInfo: 'Options: _self, _blank, _top, _parent',
+        linkClass: 'Class',
+        linkStyle: 'Style',
+        linkRel: 'Rel',
+        linkRelInfo: 'Options: alternate, author, bookmark, help, license, next, nofollow, noreferrer, prefetch, prev, search, tag',
+        linkRole: 'Role',
+      tabUpload: 'Upload',
+        upload: 'Upload',
+      tabBrowse: 'Browse',
+      editBtn: 'OK'
+    }
+  }
+});
